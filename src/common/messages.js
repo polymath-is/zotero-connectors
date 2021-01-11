2ff47a7dc68b7aded5fd7667f0bbf913bdcfb328
Adomas Venčkauskas <adomas.ven@gmail.com>
2021-01-11 12:22:22 +0200

    Changes for the 5.0.6 version of Google Docs plugin

--- src/common/messages.js
+++ src/common/messages.js
@@ -177,7 +177,9 @@ var MESSAGES = {
 		onAuthComplete: false,
 		run: {
 			background: {minArgs: 4}
-		}
+		},
+		getDocument: true,
+		batchUpdateDocument: true
 	},
 	GoogleDocsPluginManager: {
 		injectUI: true
